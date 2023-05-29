2ca17f998285b6b0ccea5781a0197cedd449c003
Nam278z01 <nam278z01@gmail.com>
2023-05-29 22:26:57 +0700

    import export

--- QLD_THD/Controllers/MonHocController.cs
+++ QLD_THD/Controllers/MonHocController.cs
@@ -1,12 +1,17 @@
 ﻿using BusinesslogicLayer;
 using DataModel;
 using DataModel.ResponseMessage;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using QLD_THD.Helper;
+using QLD_THD.CustomImport;
+using System.Data;
 
 namespace QLD_THD.Controllers
 {
@@ -14,9 +19,11 @@ namespace QLD_THD.Controllers
     [ApiController]
     public class MonHocController : ControllerBase
     {
+        IWebHostEnvironment _env;
         IMonHocBusiness _bus;
-        public MonHocController(IMonHocBusiness bus) 
+        public MonHocController(IMonHocBusiness bus, IWebHostEnvironment env) 
         {
+            _env = env ?? throw new ArgumentNullException(nameof(env));
             _bus = bus;
         }
         [Route("search")]
@@ -71,11 +78,11 @@ namespace QLD_THD.Controllers
                 if (resultBUS)
                 {
                     response.Data = model;
-                    response.MessageCode = "CreateSuccessfully";
+                    response.MessageCode = MessageCodes.CreateSuccessfully;
                 }
                 else
                 {
-                    response.MessageCode = "CreateFail";
+                    response.MessageCode = MessageCodes.CreateFail;
                 }
 
             }
@@ -97,11 +104,11 @@ namespace QLD_THD.Controllers
                 if (resultBUS)
                 {
                     response.Data = model;
-                    response.MessageCode = "UpdateSuccessfully";
+                    response.MessageCode = MessageCodes.UpdateSuccessfully;
                 }
                 else
                 {
-                    response.MessageCode = "UpdateFail";
+                    response.MessageCode = MessageCodes.UpdateFail;
                 }
             }
             catch (Exception ex)
@@ -120,11 +127,11 @@ namespace QLD_THD.Controllers
                 var resultBUS = await Task.FromResult(_bus.Delete(items));
                 if (resultBUS)
                 {
-                    response.MessageCode = "DeleteSuccessfully";
+                    response.MessageCode = MessageCodes.DeleteSuccessfully;
                 }
                 else
                 {
-                    response.MessageCode = "DeleteFail";
+                    response.MessageCode = MessageCodes.DeleteFail;
                 }
             }
             catch (Exception ex)
@@ -133,5 +140,110 @@ namespace QLD_THD.Controllers
             }
             return response;
         }
+        [Route("upload")]
+        [HttpPost, DisableRequestSizeLimit]
+
+        public async Task<IActionResult> Upload(IFormFile file)
+        {
+            try
+            {
+                if (file.Length > 0 && file.FileName.Contains(".xlsx"))
+                {
+                    var filename = Guid.NewGuid().ToString().Replace("-", "") + ".xlsx";
+                    var webRoot = _env.ContentRootPath;
+                    var filePath = Path.Combine(webRoot + "/Upload/", filename);
+                    using (var fileStream = new FileStream(filePath, FileMode.Create))
+                    {
+                        await file.CopyToAsync(fileStream);
+                    }
+                    var messageError = "";
+                    var data = ImportExcel.ReadFromExcelFileForMonHoc(filePath, 1, out messageError);
+                    var list = ExcelHelper.ConvertDataTable<MonHoc>(data);
+                    foreach (var model in list)
+                        if (!string.IsNullOrEmpty(model.MaMonHoc))
+                        {
+                            await Task.FromResult(_bus.Create(model));
+                        }
+                    return Ok(new { MessageCodes.UpdateSuccessfully });
+                }
+                else
+                {
+                    return BadRequest();
+                }
+            }
+            catch (Exception ex)
+            {
+                return Ok(new { MessageCodes.UpdateFail });
+            }
+        }
+        [Route("export-to-excel")]
+        [HttpPost]
+        public async Task<IActionResult> ExportToExcel([FromBody] Dictionary<string, object> formData)
+        {
+            try
+            {
+                long total = 0;
+                var page = int.Parse(formData["page"].ToString());
+                var pageSize = int.Parse(formData["pageSize"].ToString());
+                var mamh = formData.Keys.Contains("mamh") ? Convert.ToString(formData["mamh"]) : "";
+                var tenmh = formData.Keys.Contains("tenmh") ? Convert.ToString(formData["tenmh"]) : "";
+
+                var data = await Task.FromResult(_bus.Search(page, pageSize, mamh, tenmh, out total));
+
+                DataTable dataExport = new DataTable();
+                dataExport.Columns.Add("MaMonHoc");
+                dataExport.Columns.Add("TenMonHoc");
+                dataExport.Columns.Add("SoTiet");
+
+                List<ExcelDataExtention> staticDataValue = new List<ExcelDataExtention>();
+                staticDataValue.Add(new ExcelDataExtention
+                {
+                    IsEnd = true,
+                    StartColumnName = "C",
+                    EndColumnName = "D",
+                    StartRowIndex = 1,
+                    EndRowIndex = 1,
+                    IsMerge = true,
+                    AlignmentCenter = true,
+                    FontItalic = true,
+                    Value = "Hưng Yên, ngày " + DateTime.Now.ToString("dd") + " tháng " + DateTime.Now.ToString("MM") + " năm " + DateTime.Now.ToString("yyyy")
+                });
+                List<string> list_charge_types = new List<string>();
+                int start_row = 2;
+                if (data.Count > 0)
+                {
+                    foreach (var item in data)
+                    {
+                        DataRow row = dataExport.NewRow();
+                        row["MaMonHoc"] = item.MaMonHoc;
+                        row["TenMonHoc"] = item.TenMonHoc;
+                        row["SoTiet"] = item.SoTiet;
+                        dataExport.Rows.Add(row);
+                    }
+                }
+                else
+                {
+                    dataExport.Rows.Add();
+                }
+                var webRoot = _env.ContentRootPath;
+                var tempPath = Path.Combine(webRoot + @"\ExcelTemplates\", "mon_hoc_temp.xlsx");
+                var exportPath = Path.Combine(webRoot + @"\Export\Excel\", "mon_hoc_temp" + Guid.NewGuid().ToString() + ".xlsx");
+                string result = ExcelHelper.ExportDataTableToExcel(exportPath, tempPath, dataExport, 1, start_row + 1, staticDataValue, true, "", "", false, "");
+                if (string.IsNullOrEmpty(result))
+                {
+                    var stream = new FileStream(exportPath, FileMode.Open, FileAccess.Read);
+                    return File(stream, "application/octet-stream");
+                }
+                else
+                {
+                    throw new Exception(result);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
     }
 }
---
+++ QLD_THD/CustomImport/ImportExcel.cs
@@ -0,0 +1,91 @@
+﻿using OfficeOpenXml;
+using QLD_THD.Helper;
+using System.Data;
+using System.IO;
+using System;
+using System.Linq;
+
+namespace QLD_THD.CustomImport
+{
+    public class ImportExcel : ExcelHelper
+    {
+        public static DataTable ReadFromExcelFileForMonHoc(string path, int headerRow, out string messageError)
+        {
+            DataTable dataTable = new DataTable();
+            try
+            {
+                if (string.IsNullOrEmpty(path) || !File.Exists(path))
+                {
+                    messageError = "FILE_NOT_EXISTS";
+                    return null;
+                }
+
+                if (!string.IsNullOrEmpty(path) && Path.HasExtension(path) && Path.GetExtension(path)!.ToLower() != ".xlsx")
+                {
+                    messageError = "WRONG_FORMAT_FILE";
+                    return null;
+                }
+                ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+                using (ExcelPackage excelPackage = new ExcelPackage(new FileInfo(path)))
+                {
+                    ExcelWorkbook workbook = excelPackage.Workbook;
+                    if (workbook == null || workbook.Worksheets.Count == 0)
+                    {
+                        messageError = "EMPTY_DATA";
+                        return null;
+                    }
+
+                    ExcelWorksheet excelWorksheet = workbook.Worksheets.First();
+                    int num = 0;
+                    foreach (ExcelRangeBase item in excelWorksheet.Cells[headerRow, 1, headerRow, excelWorksheet.Dimension.End.Column])
+                    {
+                        string value = item.Text.Trim() ?? "";
+                        if (string.IsNullOrEmpty(value))
+                        {
+                            break;
+                        }
+
+                        num++;
+                        dataTable.Columns.Add(item.Text.Trim());
+                    }
+
+                    for (int i = headerRow + 1; i <= excelWorksheet.Dimension.End.Row; i++)
+                    {
+                        ExcelRange excelRange = excelWorksheet.Cells[i, 1, i, num];
+                        DataRow dataRow = dataTable.NewRow();
+                        bool flag = false;
+                        string text = "";
+                        foreach (ExcelRangeBase item2 in excelRange)
+                        {
+                            if (item2 != null)
+                            {
+                                flag = true;
+                            }
+
+                            dataRow[item2.Start.Column - 1] = item2.Value;
+                            text += ((item2.Value != null) ? item2.Value : "");
+                        }
+
+                        if (flag && !string.IsNullOrEmpty(text.Trim()))
+                        {
+                            dataTable.Rows.Add(dataRow);
+                        }
+
+                        if (string.IsNullOrEmpty(text.Trim()))
+                        {
+                            break;
+                        }
+                    }
+                }
+
+                messageError = "";
+            }
+            catch (Exception ex)
+            {
+                messageError = "ERROR:" + ex.Message;
+            }
+
+            return dataTable;
+        }
+    }
+}
